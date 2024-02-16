7b9868ddfc1f175af8180693dddc7b266acb8252
iamchandra1511 <chandra.myhome@gmail.com>
2024-02-16 18:41:09 +0530

    Spring Boot RestAPI with JPA Example

---
+++ SpringBootRestfulCRUD/src/main/java/com/onlyxcodes/app/repository/StudentRepository.java
@@ -0,0 +1,10 @@
+package com.onlyxcodes.app.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.onlyxcodes.app.model.Student;
+
+public interface StudentRepository extends CrudRepository<Student, Integer>
+{
+
+}
